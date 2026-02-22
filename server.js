04ac88683e0f4701745af8115841740bcb3f840f
akshatj0630 <jainakshat0630@gmail.com>
2026-02-22 11:07:32 +0530

    feat: Initialize database connection, define User model, and set up basic authentication routes.

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 import 'dotenv/config';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import connectDB from './src/db/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(express.static('frontend'));
+
+import authRoutes from './src/routes/auth.routes.js';
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
